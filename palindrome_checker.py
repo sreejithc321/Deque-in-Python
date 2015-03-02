c20b3abeb4a11acb32b9879f7035edd94fbab943
sreejith <sreejithc321@gmail.com>
2015-03-02 09:56:07 +0530

    palindrome checker

---
+++ palindrome_checker.py
@@ -0,0 +1,18 @@
+
+from deque import Deque
+
+def pal_check(string):
+	'''Palindrome checker using Deque'''
+	pal_dq = Deque()
+	for character in string:
+		pal_dq.add_front(character)
+	match = True
+	while(pal_dq.size()>1 and match):
+		front = pal_dq.remove_front()
+		rear = pal_dq.remove_rear()
+		if front != rear :
+			match = False
+	return match
+    
+
+print pal_check('malaaam')
